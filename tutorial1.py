cd8690bc9815d1d0977c23e5703810605668261a
rslaaf <51036680+rslaaf@users.noreply.github.com>
2019-05-26 11:31:10 +0900

    Add files via upload

---
+++ tutorial1.py
@@ -0,0 +1,43 @@
+from random import random
+
+import discord
+import random
+import fileinput
+
+token = "NTgyMDE4NzMzMjQ2MjUxMDI4.XOnu7Q.sSgi2cW7-IPGJfewUTkoSdyonB8"
+
+client = discord.Client()
+
+data = ["oppai", "tintin", "paipai", "ochinchin"]
+
+
+@client.event
+async def on_ready():
+    print("botが起動しました")
+
+@client.event
+async def on_message(message):
+    if message.content == "おはよう":
+        if client.user != message.author:
+            m = "おはよう" +message.author.name + "さん"
+
+            await message.channel.send(m)
+
+@client.event
+async def on_message(message):
+    if message.content == "/random":
+        m = (random.choice(data))
+        await message.channel.send(m)
+
+
+
+
+
+
+
+
+
+
+
+
+client.run(token)
